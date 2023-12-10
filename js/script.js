7a18d29eaf3a5a77cb7abd82720929cd185f6b7a
Hilmyys <hilmiyr123@gmail.com>
2023-12-10 21:26:12 +0700

    portfolio

---
+++ js/script.js
@@ -0,0 +1,61 @@
+
+
+
+/*========= menu navbar ==========*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+/*========= scroll sections active link ==========*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a')
+
+window.onscroll = () => {
+    // sections.forEach(sec => {
+    //     let top = window.scrollY;
+    //     let offset = sec.offsetTop - 150;
+    //     let height = sec.offsetHeight;
+    //     let id = sec.getAttribute('id');
+
+    //     if(top >= offset && top < offset + height) {
+    //         navLinks.forEach(links => {
+    //             links.classList.remove('active');
+    //             document.querySelectorAll('header nav a [href*= ' + id + ']').classList.add('active');
+    //         }); 
+    //     };
+    // });
+
+    /*========= sticky navbar ==========*/ 
+    let header = document.querySelector('.header');
+    
+    header.classList.toggle('sticky', window.scrollY > 100);
+    
+    /*========= remove menu navbar when click link ==========*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+/*========= sticky navbar ==========*/
+let darkModeIcon =document.querySelector('#drakMode-icon');
+
+darkModeIcon.onclik = () => {
+    darkModeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('drak-mode');
+};
+
+/*========= scroll reveal ==========*/
+ScrollReveal({ 
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img img, .services-container, .portfolio-box, contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
+ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
