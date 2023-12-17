0c6afc0932f46e6b52ca39bfc8a0c02020089a90
Hilmyys <hilmiyr123@gmail.com>
2023-12-17 13:24:29 +0700

    Potr

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ let drakModeIcon = document.querySelector('#darkMode-icon');
 
 drakModeIcon.onclick = () => {
     drakModeIcon.classList.toggle('bx-sun');
-    document.body.classList.toggle('darkMode-icon');
+    document.body.classList.toggle('darkMode');
 };
 
 /*========= scroll reveal ==========*/
